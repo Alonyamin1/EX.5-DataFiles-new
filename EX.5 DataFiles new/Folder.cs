c5b91aa9e486172b7e9e4c857c15bd9a47d38caa
Alonyamin1 <Alonyamin0@gmail.com>
2023-06-07 18:32:32 +0300

    with Folder class

---
+++ EX.5 DataFiles new/Folder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX._5_DataFiles_new
+{
+    class Folder
+    {
+        private string folder,path;
+        private static Folder root = new Folder("root","");
+        private AD_File[] allfolders = new AD_File[0];
+
+        public Folder(string folder,string path)
+        {
+            FolderName = folder;
+            Path = path;
+        }
+
+        public string Path
+        {
+            get { return this.path; }
+            set { this.path = value; }
+        }
+        public string FolderName
+        {
+            get { return this.folder; }
+            set
+            {
+                this.folder = value;
+            }
+        }
+    }
+}
